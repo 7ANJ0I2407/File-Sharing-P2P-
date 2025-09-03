2613283a89529c3b4629526d20ddcacc44bb948c
Sanjoy Garai <sanjoygarai@Sanjoys-MacBook-Air.local>
2025-09-03 18:09:53 +0530

    Changed1

--- Frontend/src/pages/WaitingPage.tsx
+++ Frontend/src/pages/WaitingPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react"
+import React,  { useEffect, useMemo, useState } from "react"
 import { useLocation } from "react-router-dom"
 import ProgressBar from "../components/ProgressBar"
 import { useSignal } from "../hooks/useSignal"
